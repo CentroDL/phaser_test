c71d55964b8acd40e1bd01d6b6d3a3c00392337d
Dennis Liaw <dennis.liaw@gmail.com>
2018-07-29 00:25:12 -0400

    PARCELL WOOOOO

---
+++ src/components/Game.js
@@ -0,0 +1,15 @@
+import { Game } from 'phaser';
+
+class DogTreatsGame extends Game {
+  constructor(config = {}){
+    super();
+  }
+
+  preload(){}
+
+  create(){}
+
+  update(){}
+
+  render(){}
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-index.js
+import Phaser from 'phaser';
+import DogTreatsGame from './components/Game';
+
+config = { width: 800,
+           height: 600,
+           renderer: Phaser.AUTO,
+           parent: 'game',
+           state: null,
+         }
+
+DogTreatsGame.new();
