e3d2349b46ac3a9b6a63c8fc895602fce007c15e
Dennis Liaw <dennis.liaw@gmail.com>
2018-08-05 20:38:40 -0400

    updates build settings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import DogTreatsGame from './components/Game';
 
-config = { width: 800,
+const config = { width: 800,
            height: 600,
            renderer: Phaser.AUTO,
            parent: 'game',
